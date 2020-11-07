8a0cf1c3780382b06e4eec2cebcdd0561cbd38f5
Kelsi Williams <kelsi.n.williams>
2020-11-07 16:23:02 -0500

    Added relevant icon from API

--- src/script.js
+++ src/script.js
@@ -22,6 +22,15 @@ function formatDate(timestamp) {
 
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
